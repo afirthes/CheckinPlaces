dabda50de94eb431af232be3e64404cd864a41bf
Sergey Chukanov <schukanov@gmail.com>
2022-07-24 15:32:02 +0300

    Initial Commit

---
+++ CheckinPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckinPlaces
+//
+//  Created by Afir Thes on 24.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
